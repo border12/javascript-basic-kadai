ecf4e77e093e4838a6657a4e89b581a1663b4fda
border12 <139112044+border12@users.noreply.github.com>
2023-11-22 11:42:20 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,12 @@
+var time = new Date();
+
+  var year = time.getFullYear();
+  var month= time.getMonth()+1;
+  var day = time.getDate();
+  
+  console.log(year +'年');
+  console.log(month+'月');
+  console.log(day+'日'); 
+  
+  
+
